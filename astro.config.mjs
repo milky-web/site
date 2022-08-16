101df1d1889464c227339bddab4abb73c0153eeb
tsar-boomba <itg.2048@gmail.com>
2022-08-15 23:58:51 -0400

    start on header component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import prefetch from '@astrojs/prefetch';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://milkyweb.app',
 	// Enable Preact to support Preact JSX components.
-	integrations: [preact({ compat: true })],
-	
+	integrations: [preact(), prefetch(), sitemap()],
 });
