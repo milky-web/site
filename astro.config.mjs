6187d0fe5ffe53017c3fa2c536ee2e61ff634671
tsar-boomba <itg.2048@gmail.com>
2022-08-18 11:46:19 -0400

    flamethrower router to make page transition smooth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import prefetch from '@astrojs/prefetch';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://milkyweb.app',
 	// Enable Preact to support Preact JSX components.
-	integrations: [preact(), prefetch(), sitemap()],
+	integrations: [preact(), sitemap()],
 });
---
+++ src/global.d.ts
@@ -0,0 +1,9 @@
+import type { Router } from 'flamethrower-router/dist/router';
+
+declare global {
+	interface Window {
+		router: Router;
+	}
+
+	let router: Router;
+}
