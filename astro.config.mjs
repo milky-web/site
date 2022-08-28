a43c05591f0bda59999e1ecb28e33d70eea1e58c
tsar-boomba <itg.2048@gmail.com>
2022-08-28 00:13:31 -0400

    update astro remove prefetch & preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://milkyweb.app',
 	// Enable Preact to support Preact JSX components.
-	integrations: [preact(), sitemap()],
+	integrations: [sitemap()],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,1 @@
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-	readonly DEV: boolean;
-	readonly PROD: boolean;
-	readonly MODE: 'development' | 'production';
-	readonly BASE_URL: string;
-	readonly SITE: string;
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
