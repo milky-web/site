c196a978b0fe4fad599ec53afea57342c4397a56
tsar-boomba <itg.2048@gmail.com>
2022-08-27 23:45:22 -0400

    fix logging in prod 🤮

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly DEV: boolean;
+	readonly PROD: boolean;
+	readonly MODE: 'development' | 'production';
+	readonly BASE_URL: string;
+	readonly SITE: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
