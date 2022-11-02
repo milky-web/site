52132923d9f5994eed01216b7ee6c8bfc9504874
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2022-11-02 16:27:43 -0400

    add some design stuff & refactor theme vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://milkyweb.app',
 	// Enable Preact to support Preact JSX components.
-	integrations: [sitemap()],
+	integrations: [
+		sitemap(),
+		compress(),
+	],
 });
